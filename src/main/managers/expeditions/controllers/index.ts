722beadbae8d993e90d47fc9352186d202929ed7
José García Arévalo <187457511+STWJXSX@users.noreply.github.com>
2026-04-21 18:49:08 +0200

    Delete src directory

--- src/main/managers/expeditions/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-// Exportar todos los controllers de expediciones
-export * from './sendExpedition';
-export * from './abandonExpedition';
-export * from './collectExpedition';
-export * from './RefreshExpeditions';
--- src/main/managers/expeditions/index.ts
+++
@@ -1,9 +0,0 @@
-// Exportar el manager principal de expediciones
-// Exportar el manager principal de expediciones
-import { ExpeditionManager } from './ExpeditionManager';
-import expeditionManager from './ExpeditionManager';
-
-export { ExpeditionManager, expeditionManager };
-export default expeditionManager;
-export * from './helpers';
-export * from './controllers';
